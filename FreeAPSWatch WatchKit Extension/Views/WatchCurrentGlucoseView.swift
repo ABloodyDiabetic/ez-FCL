28f84db6c06b456493d6c403da307788ad66d726
ABloodyDiabetic <lerwin2696@outlook.com>
2024-05-11 10:41:07 -0500

    Updated watch app UI to match iOS version

---
+++ FreeAPSWatch WatchKit Extension/Views/WatchCurrentGlucoseView.swift
@@ -0,0 +1,18 @@
+//
+//  WatchCurrentGlucoseView.swift
+//  FreeAPSWatch WatchKit Extension
+//
+//  Created by Lester Erwin on 5/11/24.
+//
+
+import SwiftUI
+
+struct WatchCurrentGlucoseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WatchCurrentGlucoseView()
+}
