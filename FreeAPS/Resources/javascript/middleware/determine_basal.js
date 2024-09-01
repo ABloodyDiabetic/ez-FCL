7de0e9afc00a5ad6f3e791632c7118179f8f23fd
ABloodyDiabetic <101060197+ABloodyDiabetic@users.noreply.github.com>
2024-09-01 03:02:55 -0500

    Update determine_basal.js (Remove space)

--- FreeAPS/Resources/javascript/middleware/determine_basal.js
+++ FreeAPS/Resources/javascript/middleware/determine_basal.js
@@ -1,5 +1,5 @@
 function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock, pumphistory, preferences, basalprofile) {
     // modify anything
-    // return any reason what has changed. 
+    // return any reason what has changed.
     return "Nothing changed";
 }
